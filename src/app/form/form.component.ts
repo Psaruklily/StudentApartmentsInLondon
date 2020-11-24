cad473c7c38ff8907182d3cbb67709523fc804c4
Psaruklily <psarukli96@gmail.com>
2020-11-24 19:06:15 +0300

    Validated phone input in form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -14,15 +14,17 @@ currentStudent: FormGroup;
 
   constructor() { 
     this.currentStudent = new FormGroup({
-      "studentFirstName": new FormControl('Lilya', Validators.required)
+      "studentFirstName": new FormControl('Lilya', Validators.required),
+      "studentLastName": new FormControl('Psaruk', Validators.required),
+      "studentPhone": new FormControl('', Validators.pattern("[\(]{0,1}[\+][0-9]{3}[\)]{0,1}[ ]?[0-9]{2}[-][0-9]{3}[-][0-9]{2}[-][0-9]{2}")),
+      "studentEmail": new FormControl('', Validators.required)
     })
   }
-
+  
   ngOnInit(): void {
   }
 
   submit(){
     console.log(this.currentStudent);
   }
-
 }
